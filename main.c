fe8c89eac7ea8eb43534a9035def3b87d482a2a9
Vlad Prudius <vladvlpr006@gmail.com>
2024-05-16 10:44:40 +0300

    refactor main.c to command pattern

--- main.c
+++ main.c
@@ -1,63 +1,128 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include "gap_buffer.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+typedef void (*CommandFunc)(GapBuffer *, bool *);
+
+typedef struct {
+    const char *name;
+    CommandFunc func;
+} Command;
+
 void print_help() {
     printf("Available commands:\n");
-    printf("1. Append text symbols to the end\n");
-    printf("2. Start a new line\n");
-    printf("3. Save text to a file\n");
-    printf("4. Load text from a file\n");
-    printf("5. Print the current text to console\n");
-    printf("6. Insert text by line and symbol index\n");
-    printf("7. Search for text\n");
-    printf("8. Clear console\n");
-    printf("0. Exit\n");
+    printf("help - Print this help message\n");
+    printf("exit - Exit the program\n");
+    printf("append - Append text to the end of the buffer\n");
+    printf("nl - Start a new line\n");
+    printf("save - Save the buffer to a file\n");
+    printf("load - Load a file into the buffer\n");
+    printf("print - Print the contents of the buffer\n");
+    printf("insert - Insert text at a specific line and index\n");
+    printf("search - Search for text in the buffer\n");
+    printf("clear - Clear the console\n");
 }
 
-void handle_input(GapBuffer *gb, const char *command, bool *is_running) {
-    char text[256];
-    char filename[256];
-    int line, index;
+void cmd_help(GapBuffer *gb, bool *is_running) {
+    print_help();
+}
 
-    if (strcmp(command, "0") == 0 || strcmp(command, "exit") == 0) {
-        gap_buffer_free(gb);
-        *is_running = false;
-    } else if (strcmp(command, "1") == 0) {
-        printf("Enter text to append: ");
-        scanf(" %[^\n]", text);
-        gap_buffer_insert_text(gb, text);
-    } else if (strcmp(command, "2") == 0) {
-        printf("New line is started\n");
-        gap_buffer_insert_text(gb, "\n");
-    } else if (strcmp(command, "3") == 0) {
-        printf("Enter the file name for saving: ");
-        scanf("%s", filename);
-        gap_buffer_save_to_file(gb, filename);
-    } else if (strcmp(command, "4") == 0) {
-        printf("Enter the file name for loading: ");
-        scanf("%s", filename);
-        gap_buffer_load_from_file(gb, filename);
-    } else if (strcmp(command, "5") == 0) {
-        gap_buffer_print(gb);
-    } else if (strcmp(command, "6") == 0) {
-        printf("Choose line and index: ");
-        scanf("%d %d", &line, &index);
-        printf("Enter text to insert: ");
-        scanf(" %[^\n]", text);
-        gap_buffer_insert_at(gb, line, index, text);
-    } else if (strcmp(command, "7") == 0) {
-        printf("Enter text to search: ");
-        scanf(" %[^\n]", text);
-        gap_buffer_search(gb, text);
-    } else if (strcmp(command, "8") == 0) {
-        system("cls");
-    } else {
-        printf("The command is not implemented yet :(\n");
-        print_help();
+void cmd_exit(GapBuffer *gb, bool *is_running) {
+    gap_buffer_free(gb);
+    *is_running = false;
+}
+
+void cmd_append(GapBuffer *gb, bool *is_running) {
+    char text[256];
+    printf("Enter text to append: ");
+    scanf(" %[^\n]", text);
+    gap_buffer_insert_text(gb, text);
+}
+
+void cmd_newline(GapBuffer *gb, bool *is_running) {
+    printf("New line is started\n");
+    gap_buffer_insert_text(gb, "\n");
+}
+
+void cmd_save(GapBuffer *gb, bool *is_running) {
+    char filename[256];
+    printf("Enter the file name for saving: ");
+    scanf("%s", filename);
+    gap_buffer_save_to_file(gb, filename);
+}
+
+void cmd_load(GapBuffer *gb, bool *is_running) {
+    char filename[256];
+    printf("Enter the file name for loading: ");
+    scanf("%s", filename);
+    gap_buffer_load_from_file(gb, filename);
+}
+
+void cmd_print(GapBuffer *gb, bool *is_running) {
+    gap_buffer_print(gb);
+}
+
+void cmd_insert(GapBuffer *gb, bool *is_running) {
+    char text[256];
+    int line, index;
+    printf("Choose line and index: ");
+    scanf("%d %d", &line, &index);
+    printf("Enter text to insert: ");
+    scanf(" %[^\n]", text);
+    gap_buffer_insert_at(gb, line, index, text);
+}
+
+void cmd_search(GapBuffer *gb, bool *is_running) {
+    char text[256];
+    printf("Enter text to search: ");
+    scanf(" %[^\n]", text);
+    gap_buffer_search(gb, text);
+}
+
+void cmd_clear(GapBuffer *gb, bool *is_running) {
+    system("cls");
+}
+
+Command commands[] = {
+    {"help", cmd_help},
+    {"exit", cmd_exit},
+    {"append", cmd_append},
+    {"nl", cmd_newline},
+    {"save", cmd_save},
+    {"load", cmd_load},
+    {"print", cmd_print},
+    {"insert", cmd_insert},
+    {"search", cmd_search},
+    {"clear", cmd_clear}
+};
+
+void handle_input(GapBuffer *gb, const char *command, bool *is_running) {
+    for (int i = 0; i < sizeof(commands) / sizeof(Command); i++) {
+        if (strcmp(command, commands[i].name) == 0) {
+            commands[i].func(gb, is_running);
+            return;
+        }
     }
+
+    printf("The command is not implemented yet :(\n");
+    print_help();
+}
+
+char *trim_whitespace(char *str) {
+    while(isspace(*str)) str++;
+
+    if(*str == 0)
+        return str;
+
+    char *end = str + strlen(str) - 1;
+    while(end > str && isspace(*end)) end--;
+
+    end[1] = '\0';
+
+    return str;
 }
 
 int main(void) {
@@ -69,9 +134,9 @@ int main(void) {
 
     while (is_running) {
         char command[256];
-        printf("Choose the command: ");
+        printf("[EDitor]$: ");
         scanf("%s", command);
-        handle_input(gb, command, &is_running);
+        handle_input(gb, trim_whitespace(command), &is_running);
     }
 
     return 0;
